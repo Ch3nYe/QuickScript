1b68a750ab28908987840be06b903b985aa76bcc
ChenYe <51895486+Ch3nYe@users.noreply.github.com>
2019-09-25 08:28:41 +0800

    Update pingtool.py

--- python/pingtool/pingtool.py
+++ python/pingtool/pingtool.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python
 #auther: chenye
-# read ip list file and ping 
+
 import os,sys
 import subprocess,cmd
 
 def subping():
     f = open("ip_list.txt","r")
-
+    fout = open("tong.txt","a+")
     lines = f.readlines()
     for line in lines:
         # print(line)
-        ret = os.popen("ping -c 2 -w 5 %s" % line).read()
+        ret = os.popen("ping -c 2 -w 2 %s" % line).read()
         if "ttl" in ret:
             print("\033[32m[+]"+line+"\033[0m")
+            fout.write(line+"\n")
         else:
             print("\033[31m[-]"+line+"\033[0m")
     f.close()
-
+    fout.close()
 subping()
