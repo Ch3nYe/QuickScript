2d1621dc9541a76f579bdfcbff254be5f4767f17
ChenYe <51895486+Ch3nYe@users.noreply.github.com>
2019-12-08 20:55:00 +0800

    spiderurls.py

---
+++ python/spiderurls.py
@@ -0,0 +1,25 @@
+from requests import request
+from lxml import etree
+urls = []
+
+for i in range(1,11):
+    url = "https://scss.bupt.edu.cn/szdw/jsml/%s.htm" % (str(i))
+    response = request(url=url, method='GET')
+    response = response.content.decode('utf-8')
+    html = etree.HTML(response)
+
+    gets = html.xpath('/html/body/div[3]/div[2]/div[2]/div/ul//li/div/a/@href')
+    # print("\n".join(gets))
+    urls.append()
+
+# 第一页
+response = request(url="https://scss.bupt.edu.cn/szdw/jsml.htm", method='GET')
+response = response.content.decode('utf-8')
+html = etree.HTML(response)
+gets = html.xpath('/html/body/div[3]/div[2]/div[2]/div/ul//li/div/a/@href')
+# print("\n".join(gets))
+urls.append(gets)
+
+print(len(urls))
+
+# url = html.xpath('/html/body/div[3]/div[2]/div[2]/div/ul//li/a/@href')
